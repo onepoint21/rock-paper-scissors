7934d34a3ecc30bcc83036934ae07cbb2f378352
Jack White <jackhwhite.83@gmail.com>
2025-02-04 18:31:53 -0700

    Implement cpu logic in js

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ let userChoice = 0;
 // Declare a placeholder for computer choice
 let cpuChoice = 0;
 
-
+// Get and store user choice
 userChoice = prompt();
+let userResult = userChoice.toString()
 
-console.log(userChoice);
+console.log(userResult);
 
+// Get and store cpu choice
 function getCpuChoice() {
-    cpuChoice = Math.floor(Math.random() * 3) + 1;
-    return cpuChoice;
+    return Math.floor(Math.random() * 3) + 1;
 }
 
-let cpuResult = getCpuChoice(cpuChoice);
+cpuChoice = getCpuChoice();
+let cpuValue = ["Rock", "Paper", "Scissors"];
+let cpuResult = cpuValue[cpuChoice - 1];
 
 console.log(cpuResult);
